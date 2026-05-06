c8bc9bb189e5c688a214a4ebd2402b14e6b4c892
Victormanzou <manzovt@hotmail.com>
2026-05-06 14:45:35 -0600

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("ventas.html")
+    return "<h1>FUNCIONA BIEN</h1>"
